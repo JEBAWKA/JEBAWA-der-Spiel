8738e90e184dc6ad9239178b32808fc3c7d00cc3
Jakub Moliński <kubamolinski@gmail.com>
2015-08-24 15:04:25 +0200

    Update hex.h

--- map/hex.h
+++ map/hex.h
@@ -4,7 +4,7 @@
 #include <list>
 #include "coord.h"
 
-enum class : char TERRAIN_TYPE
+enum class TERRAIN_TYPE : char
 {
     LAKE = 0,
     FOREST = 1,
@@ -31,37 +31,15 @@ class Hex
 
 public:
     Hex(Coord coord) : coord(coord) {} // konstruktor
-    void add_tag(TERRAIN_TYPE tag)
-    {
-        tags.push_front(tag);
-        tags.unique();
-    }
-    void remove_tag(TERRAIN_TYPE tag)
-    {
-        tags.remove(tag);
-    }
-    Coord get_coord()
-    {
-        return this->coord;
-    }
-    /*void add_building(Building bld)
-    {
-        buildings.push_front(bld);
-        buildings.unique();
-    }
-    void remove_building(Building bld)
-    {
-        buildings.remove(bld);
-    }
-    void add_event(Event event)
-    {
-        active_events.push_front(event);
-        active_events.unique();
-    }
-    void remove_event(Event event)
-    {
-        active_events.remove(event);
-    }*/
+    void add_tag(TERRAIN_TYPE tag);
+    void remove_tag(TERRAIN_TYPE tag);
+    Coord get_coord();
+    /*
+    void add_building(Building bld);
+    void remove_building(Building bld);
+    void add_event(Event event);
+    void remove_event(Event event);
+    */
 };
 
 #endif // HEX_H
