55eaa550f6ad0591f70225c73bed88ce5a0cfbc3
Glen <kubamolinski@gmail.com>
2015-08-26 12:51:31 +0200

    dodane kilka metod mapy

--- map/hex_map.cpp
+++ map/hex_map.cpp
@@ -10,7 +10,7 @@ void Hex_map::set_size(int x, int y)
     this->map.resize(y, row);
 }
 
-Hex_map::Hex_map(int x, int y)
+Hex_map::Hex_map(int x, int y) : size_x(x), size_y(y)
 {
     this->set_size(x,y);
 }
@@ -61,13 +61,6 @@ std::shared_ptr<Hex> Hex_map::find_neighbour_ptr(std::shared_ptr<Hex> hex, Coord
     return find_neighbour_ptr(hex->get_coord(), direction);
 }
 
-/*
-std::vector<std::shared_ptr<Hex>> Hex_map::get_neighbours(Hex hex) // chyba niepotrzebna
-{
-
-}
-*/
-
 Hex Hex_map::get_hex_copy(Coord coord) // lepiej nie uzywac
 {
     return *map[coord.x][coord.y].second;
@@ -87,8 +80,38 @@ bool Hex_map::remove_hex(Coord coord)
     {
         return false; // nie mozna usunac nieistniejacego obiektu
     }
+    hexes.remove(get_hex_copy(coord)); // usuwa wlasciwy obiekt z listy
     map[coord.x][coord.y].first = false; // ustawia flage na puste pole
-    map[coord.x][coord.y].second.reset(); // usuwa trzymany obiekt
+    map[coord.x][coord.y].second.reset(); // resetuje wskaznik
     map[coord.x][coord.y].second = nullptr; // i ustawia na nullptr
     return true;
+}
+
+std::vector<std::shared_ptr<Hex>> Hex_map::get_neighbours(Coord hex) // podstawowa
+{
+    // TODO napisz to
+    std::vector<std::shared_ptr<Hex> neigbours = {};
+    return neigbours;
+}
+
+std::vector<std::shared_ptr<Hex>> Hex_map::get_neighbours(Hex hex)
+{
+    return get_neighbours(hex.get_coord());
+}
+
+std::vector<std::shared_ptr<Hex>> Hex_map::get_neighbours(std::shared_ptr<Hex> hex)
+{
+    return get_neighbours(hex->get_coord());
+}
+
+bool Hex_map::is_coord_within_map(Coord coord) // czy dane koordynaty naleza do planszy
+{
+    return (coord.x >= 0 && coord.y >= 0 &&        // koordynaty nie moga byc ujemne
+            coord.x < size_x && coord.y < size_y); // koordynaty nie moga byc wieksze ani rowne
+                                                   // od wielkosci planszy
+}
+
+bool Hex_map::hex_exists(Coord coord) // czy hex o podanych koordynatach istnieje
+{
+    return map[coord.x][coord.y].first; // zwraca flage informujaca czy hex istnieje
 }
--- map/hex_map.h
+++ map/hex_map.h
@@ -10,19 +10,22 @@
 
 class Hex_map
 {
+    int size_x, size_y;
     std::vector<std::vector<std::pair<bool, std::shared_ptr<Hex>>>> map;
     std::list <Hex> hexes;
     Hex get_hex_copy(Coord coord); // lepiej nie uzywac
-    Coord find_neighbour(Coord hex, Coord direction); // niebezpieczna - nie wiadomo czy hex istnieje
-                                                      // dlatego uzytkownik nie moze sam jej wywolac
-                                                      // podstawowa
-    std::shared_ptr<Hex> find_neighbour_ptr(Coord hex, Coord direction); // niebezpieczna - nie wiadomo czy hex istnieje
-                                                                         // dlatego uzytkownik nie moze sam jej wywolac
-                                                                         // podstawowa
+
+    // 2 niebezpieczne metody - nie wiadomo czy hex istnieje
+    // dlatego uzytkownik nie moze sam ich wywolac
+    Coord find_neighbour(Coord hex, Coord direction); // podstawowa
+    std::shared_ptr<Hex> find_neighbour_ptr(Coord hex, Coord direction); // podstawowa
+
+    bool is_coord_within_map(Coord coord); // czy dane koordynaty naleza do planszy
+    bool hex_exists(Coord coord); // czy hex o podanych koordynatach istnieje
 public:
-    void set_size(int x,int y);
+    void set_size(int x, int y);
     Hex_map();
-    Hex_map(int x,int y);
+    Hex_map(int x, int y);
     std::shared_ptr<Hex> add_hex(Coord where); //Zwraca wskaznik na dodanego heksa.
     bool remove_hex(Coord where); //Zwraca czy udalo sie usunac;
     int distance(Coord first, Coord second); // podstawowa
@@ -32,6 +35,13 @@ public:
     Coord find_neighbour(std::shared_ptr<Hex> hex, Coord direction);
     std::shared_ptr<Hex> find_neighbour_ptr(Hex hex, Coord direction);
     std::shared_ptr<Hex> find_neighbour_ptr(std::shared_ptr<Hex> hex, Coord direction);
+
+    // tych lepiej nie uzywac
+    std::vector<std::shared_ptr<Hex>> get_neighbours(Coord hex); // podstawowa
+    std::vector<std::shared_ptr<Hex>> get_neighbours(Hex hex);
+    std::vector<std::shared_ptr<Hex>> get_neighbours(std::shared_ptr<Hex> hex);
+
+    std::shared_ptr<Hex> get_hex_ptr(Coord coord);
 };
 
 #endif // HEX_MAP_H
